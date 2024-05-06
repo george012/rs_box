478e6cf8d622513fe2371dda22750e9c6bdacc58
WMYeah_Mac <george012@163.com>
2024-05-06 15:51:55 +0800

    Update v0.0.11

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, allow(unused_attributes))]
+#![doc = include_str!("../README.md")]
+#![allow(non_upper_case_globals)]
+#![allow(clippy::needless_doctest_main)]
+#![warn(missing_docs)]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::unnecessary_cast)]
+#![allow(clippy::arc_with_non_send_sync)]
+
 mod lib_tests;
 pub mod rs_box_log;
 
